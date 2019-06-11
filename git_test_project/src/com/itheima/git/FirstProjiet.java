63ab811854f6e6dcec8a345610606c1ad8d74c4a
zarro <zarro@itcast>
2019-06-11 22:06:57 +0800

    修改java工程

--- git_test_project/src/com/itheima/git/FirstProjiet.java
+++ git_test_project/src/com/itheima/git/FirstProjiet.java
@@ -8,9 +8,11 @@ public class FirstProjiet {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //test git
     }
     public void test(){
         int i=0;
         i+=10;
+        System.out.println(i);
     }
 }
