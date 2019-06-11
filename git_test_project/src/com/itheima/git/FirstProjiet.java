f601666d10dcbd52741c9a913d4eafce85ab3588
zarro <zarro@itcast>
2019-06-11 21:34:41 +0800

    将java工程添加到git

---
+++ git_test_project/src/com/itheima/git/FirstProjiet.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+public class FirstProjiet {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
